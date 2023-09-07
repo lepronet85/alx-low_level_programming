3131cd649e4b27d9f05cb97b2abfed1f64760561
Abdoul azize <lepronet85@gmail.com>
2023-09-07 14:31:54 -0700

    Add 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-/* Printing a text with puts function  */
+/**
+ * main - main entry of the program
+ * This program demonstrates the use of the puts function.
+ * Return: Return 0 to stop the program
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
