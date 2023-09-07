da8d6ab570e7c668b640a6ff2fbdafe920fd4d80
Abdoul azize <lepronet85@gmail.com>
2023-09-07 14:42:05 -0700

    Add 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - Main program entry
  * Print the text passed to the printf function
- * Return 0 to stop the program
+ * Return: Return 0 to stop the program
  */
-int main(void) 
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
- 	return (0);
+	return (0);
 }
