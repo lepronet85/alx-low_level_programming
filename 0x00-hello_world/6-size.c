4a110ef705e77630634ad23a69d6307c9e33b8f2
Abdoul azize <lepronet85@gmail.com>
2023-09-07 15:00:50 -0700

    Add 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,14 +3,14 @@
 /**
  * main - Main program entry
  * Print the text passed to the printf function
- * Return 0 to stop the program
+ * Return: Return 0 to stop the program
  */
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: 8 byte(s) %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: 8 byte(s) %d byte(s)\n", sizeof(float));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
