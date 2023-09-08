2a9150e91c6eba534fc01ce99935b2bbee70a2bf
Abdoul azize <lepronet85@gmail.com>
2023-09-08 14:13:14 -0700

    Add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ * Print the alphabet in lowercase
+ * Return: Return 0 to stop the program
+ */
+int main(void)
+{
+	for (int i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
