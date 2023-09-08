003cffec900880c0f61109e3354b3f01ee7b3f02
Abdoul azize <lepronet85@gmail.com>
2023-09-08 16:16:27 -0700

    Add 6-print_numbersz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ * Prints all single digit numbers of base 10 starting from 0
+ * Return: Return 0 to stop the program
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
