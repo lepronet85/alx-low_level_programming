a287176be9f7c49bd906539e294e02c617a39940
Abdoul azize <lepronet85@gmail.com>
2023-09-12 19:23:26 -0700

    Add 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,15 +8,28 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, result;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			_putchar('0' + i * j);
-			_putchar(',');
-			_putchar(' ');
+			result = i * j;
+			if (j == 0)
+			{
+				_putchar('0' + result);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (result < 10)
+					_putchar(' ');
+
+				_putchar('0' + result);
+			}
+
 		}
 
 		_putchar('\n');
