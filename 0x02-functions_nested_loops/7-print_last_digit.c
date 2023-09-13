0ee8b0c0d48b1ffd6a06cb02b6247126372202c5
Abdoul azize <lepronet85@gmail.com>
2023-09-12 17:26:45 -0700

    Add 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Function to print lastdigit of number
+ * @n: The number
+ * Description: This function print last digit of number passed
+ * Return: return last digit of number
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -65,4 +65,13 @@ int print_sign(int n);
 
 int _abs(int n);
 
+/**
+ * print_last_digit - Function to print lastdigit of number
+ * @n: The number
+ * Description: This function print last digit of number passed
+ * Return: return last digit of number
+ */
+
+int print_last_digit(int n);
+
 #endif
