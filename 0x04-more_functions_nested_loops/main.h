7c40fa92028e2438f3f246f778823c45c8164aed
Abdoul azize <lepronet85@gmail.com>
2023-09-14 14:53:40 -0700

    Add 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - Similar to putchar from stdio
+ * @c: The character to be written
+ * Description: similar to putchar from stdio
+ * Return: None (void)
+ */
+
+void _putchar(char c);
+
+/**
+ * _isupper - Function that check if upper
+ * @c: The letter to check
+ * Description: This function checks if letter passed is
+ * uppercase character
+ * Return: Reurn 1 or 0
+ */
+
+int _isupper(int c);
+
+#endif
