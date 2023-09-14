4fd98f20fd308a938734bc4740346199d894a3cf
Abdoul azize <lepronet85@gmail.com>
2023-09-13 17:18:30 -0700

    Add 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,6 @@ void print_times_table(int n)
 
 	if (n < 0 || n > 15)
 		return;
-
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
@@ -28,13 +27,14 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				if (result <= 10)
+
+				if (result < 10)
 				{
 					_putchar(' ');
 					_putchar('0' + result);
+					_putchar('0' + result);
 				}
-				else if (result >= 10 && result < 100)
+				else if (result < 100)
 				{
 					_putchar('0' + result / 10);
 					_putchar('0' + result % 10);
