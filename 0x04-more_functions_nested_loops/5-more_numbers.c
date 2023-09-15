b8625018df366a5f6439d9ff335ad2a292f7dcb5
Abdoul azize <lepronet85@gmail.com>
2023-09-14 18:04:27 -0700

    Add 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,10 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
-			_putchar('0' + j);
+			if (j >= 10)
+				_putchar('0' + j / 10);
 
-		_putchar('\n');
+		_putchar('0' + j % 10);
 	}
 
 	_putchar('\n');
