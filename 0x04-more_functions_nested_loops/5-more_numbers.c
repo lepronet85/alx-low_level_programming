4d4ba80f4635518618170d0321e1204855711e9f
Abdoul azize <lepronet85@gmail.com>
2023-09-14 18:07:35 -0700

    Add 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,12 +14,12 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
+		{
 			if (j >= 10)
 				_putchar('0' + j / 10);
-
-		_putchar('0' + j % 10);
+			_putchar('0' + j % 10);
+		}
+		_putchar('\n');
 	}
-
-	_putchar('\n');
 
 }
