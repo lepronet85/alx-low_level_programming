c6e06f54b56d139a9e52db48cd481b99680969a2
Abdoul azize <lepronet85@gmail.com>
2023-09-15 10:16:58 -0700

    Add 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,22 +9,32 @@
 
 void print_number(int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
+	int div = 1;
+	int print_flag = 0;
 
 	if (n < 0)
 	{
 		_putchar('-');
+		if (n == -2147483648)
+		{
+			n += 1;
+			print_flag = 1;
+		}
 		n = -n;
 	}
 
-	if (n / 10 != 0)
-	{
-		print_number(n / 10);
-	}
+	for (div = 1; div <= n / 10; div *= 10)
+	;
 
-	_putchar((n % 10) + '0');
+	while (div > 0)
+	{
+		int digit = (n / div) % 10;
+
+		if (digit != 0 || print_flag || div == 1)
+		{
+			_putchar(digit + '0');
+			print_flag = 1;
+		}
+		div /= 10;
+	}
 }
