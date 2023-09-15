246accdb840942be8268c47c37be8a52efaaa226
Abdoul azize <lepronet85@gmail.com>
2023-09-15 12:03:10 -0700

    Add 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Fonction principale du programme
+ * Description : Cette fonction calcule la somme de tous les multiples de 3 ou 5
+ * inférieurs à 1024 (exclus), puis affiche le résultat.
+ * Return: 0 si le programme s'exécute avec succès
+ */
+
+int main(void)
+{
+	int sum = 0;
+	int limit = 1024;
+
+	for (int i = 3; i < limit; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return 0;
+}
