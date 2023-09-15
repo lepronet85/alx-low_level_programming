f93bd96ee32ab11d4213dc44c3e033597c6f59c3
Abdoul azize <lepronet85@gmail.com>
2023-09-15 09:07:15 -0700

    Add 9-fiz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * main - Program entry point
+ * Descrition: This program print Fizbuzz
+ * Return: 0 to stop program
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i < 100)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+
+	return (0);
+}
