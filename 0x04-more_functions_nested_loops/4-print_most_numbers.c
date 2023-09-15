96a1e1b63acfd47650cc95ce4d3d25d8e5d2a8b0
Abdoul azize <lepronet85@gmail.com>
2023-09-14 17:42:53 -0700

    Add 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * print_most_numbers - Function that prints numbers, from 0 to 9
+ * Description: This function prints numbers from 0 to 9 without
+ * 2 and 4
+ * Return: None (void)
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++) {
+		if (i != 2 && i != 4)
+			_putchar(i);
+	}
+
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -50,4 +50,13 @@ int mul(int a, int b);
 
 void print_numbers(void);
 
+/**
+ * print_most_numbers - Function that prints numbers, from 0 to 9
+ * Description: This function prints numbers from 0 to 9 without
+ * 2 and 4
+ * Return: None (void)
+ */
+
+void print_most_numbers(void);
+
 #endif
