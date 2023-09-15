1cac53f771bf6e14b028fe49682334c3d863f9b6
Abdoul azize <lepronet85@gmail.com>
2023-09-14 18:42:12 -0700

    Add 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,14 +10,14 @@
 
 void print_square(int size)
 {
+	int row, col;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int row, col;
-
 		for (row = 0; row < size; row++)
 		{
 			for (col = 0; col < size; col++)
