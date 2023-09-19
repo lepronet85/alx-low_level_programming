aae50e12812a3f398cf454ac25524923e9117c89
Abdoul azize <lepronet85@gmail.com>
2023-09-18 19:02:53 -0700

    Add 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,8 +14,14 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (a == NULL || n <= 0)
+	if (a == NULL)
 		return;
+
+	if (a <= 0)
+	{
+		printf("\n");
+		return;
+	}
 
 	for (i = 0; i < n; i++)
 	{
