0fcd695fb096530816e37c30fe5d1720c4e5d639
Abdoul azize <lepronet85@gmail.com>
2023-09-18 18:25:43 -0700

    Add 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,14 +12,14 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
 	if (str == NULL)
 		return;
 
-	while (str[i] != '\0') {
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
 		_putchar(str[i]);
-		i += 2;
 	}
 
 	_putchar('\n');
