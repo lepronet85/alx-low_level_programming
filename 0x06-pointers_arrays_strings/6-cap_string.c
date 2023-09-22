d2da6ad44716de4a654815bd36ebb97aaa13c4d7
Abdoul azize <lepronet85@gmail.com>
2023-09-21 19:00:21 -0700

    Add 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdbool.h>
+#include <ctype.h>
+
+/** is_separator - Function that checks separator
+ * @c: The charcater
+ * Description: This function checks separator
+ * Return: true or false
+ */
+
+bool is_separator(char c)
+{
+	char separators[] = " \t\n,;.!?\"(){}";
+	int i;
+
+	for (i = 0; separators[i] != '\0'; i++)
+	{
+		if (c == separators[i])
+			return (true);
+	}
+	
+	return false;
+}
+
+/**
+ * cap_string - Function that capitalizes all words of a string
+ * @str: The string
+ * Description: This function capitalizes all words of a string
+ * Return: Capilazes string
+ */
+
+char *cap_string(char *str)
+{
+	bool new_word = true;
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (is_separator(*ptr))
+			new_word = true;
+		else if (new_word)
+		{
+			*ptr = toupper(*ptr);
+			new_word = false;
+		}
+		else
+			*ptr = tolower(*ptr);
+		ptr++;
+	}
+
+	return (str);
+}
