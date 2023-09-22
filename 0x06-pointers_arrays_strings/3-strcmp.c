cde4cfb18a707e3d7972be3755c70263076010f6
Abdoul azize <lepronet85@gmail.com>
2023-09-21 17:39:02 -0700

    fixing 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,9 +16,9 @@ int _strcmp(char *s1, char *s2)
 	while (*s1 != '\0' && *s2 != '\0')
 	{
 		if (*s1 < *s2)
-			return (-1);
+			return (-15);
 		else if (*s1 > *s2)
-			return (1);
+			return (15);
 
 		s1++;
 		s2++;
