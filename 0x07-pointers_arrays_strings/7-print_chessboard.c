e35ab2ea7bef332c6f769acb6454c1a9aa6e49d8
Abdoul azize <lepronet85@gmail.com>
2023-09-25 19:29:05 -0700

    Add 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,17 +9,12 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int size = sizeof(*a) / sizeof(a[0]);
-	int i;
-	int j;
+	int i, j;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; a[i][7]; i++)
 	{
-		for (j = 0; j < size; j++)
-		{
+		for (j = 0; j < 8; j++)
 			_putchar(a[i][j]);
-		}
-
 		_putchar('\n');
 	}
 }
