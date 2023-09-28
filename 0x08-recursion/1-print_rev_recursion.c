26b02db7ce870d8f766f092f56724508f3eb4a47
Abdoul azize <lepronet85@gmail.com>
2023-09-28 16:36:40 -0700

    Add 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Function that prints a string in reverse, followed
+ * by a new line.
+ * @s: The string
+ * Description: This function prints a string in reverse, followed by a new line.
+ * Return: None (void)
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_puts_recursion(s + 1);
+	+putchar(s[0]);
+}
