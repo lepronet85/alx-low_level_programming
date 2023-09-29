4961dba4ecf5ced3eed9189fcbf9dce7b58d9398
Abdoul azize <lepronet85@gmail.com>
2023-09-28 17:41:27 -0700

    5-sqrt_recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function that returns the naturaL
+ * square root of a number.
+ * @n: The number
+ * Description: This function  returns the natural square
+ * root of a number.
+ * Return: The sqrt value
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n == 0 || n == 1)
+		return (n);
+
+	return (_sqrt_helper(n, 1, n));
+}
+
+/**
+ * _sqrt_helper - function that returns the naturaL
+ * square root of a number.
+ * @n: The number
+ * @start: The start
+ * @end: The end
+ * Description: This function  returns the natural square
+ * root of a number.
+ * Return: The sqrt value
+ */
+
+int _sqrt_helper(int n, int start, int end)
+{
+	int mid = (start + end) / 2;
+	int square = mid * mid;
+
+	if (start > end)
+		return (-1);
+
+	if (square == n)
+		return (mid);
+	else if (square < n)
+		return (_sqrt_helper(n, mid + 1, end));
+
+	return (_sqrt_helper(n, start, mid - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,4 +13,6 @@ int factorial(int n);
 
 int _pow_recursion(int x, int y);
 
+int _sqrt_recursion(int n);
+
 #endif
