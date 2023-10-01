b91683a73c45fa3e55710bac1732cc1288c1ef79
Abdoul azize <lepronet85@gmail.com>
2023-10-01 08:56:15 -0700

    Add 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int wildcm - Function to compare two strings considering
+ * wildcmp - Function to compare two strings considering
  * special char *
  * @s1: The first string
  * @s2: The second string
@@ -15,8 +15,11 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 
-	if (*s1 == *s2 || *s2 == '*')
-		return (wildcmp(s1 + 1, s2 + 1) || wildcmp(s1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
 
 	return (0);
 }
