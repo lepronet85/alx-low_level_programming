67f9c0a846c19b3e599384e56c79898633544d03
Abdoul azize <lepronet85@gmail.com>
2023-10-04 17:16:04 -0700

    Add 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,10 +10,12 @@
  */
 void free_grid(int **grid, int height)
 {
+	int i;
+
 	if (grid == NULL || height <= 0)
 		return;
 
-	for (int i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 		free(grid[i]);
 
 	free(grid);
