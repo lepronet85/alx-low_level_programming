1594f23d779f99ab950af0eecf06658efcedada5
Abdoul azize <lepronet85@gmail.com>
2023-10-07 13:47:41 -0700

    Add 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program main entry point
+ * @argc: length of program arguments
+ * @argv: Array that contains program arguments and it name
+ * Return: None (void)
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	int i = -1;
+
+	while(argc--)
+		i++;
+	printf("%d\n", i);
+	return (0);
+}
