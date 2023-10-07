3d2690c003d9cd6d2d412ba41fae1e9314ae3208
Abdoul azize <lepronet85@gmail.com>
2023-10-07 13:28:20 -0700

    Add 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
  * Return: None (void)
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-	printf("\s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
