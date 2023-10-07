770b853bfdce7f0fde0b4af6c3f9d10cc52ba785
Abdoul azize <lepronet85@gmail.com>
2023-10-07 15:24:21 -0700

    Update

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _atoi - converts a string to an int
+* @s: the string to be changed
+* Description: This function a string to an int
+* Return: the converted int
+*/
+
+int _atoi(char *s)
+{
+	return (0);
+}
