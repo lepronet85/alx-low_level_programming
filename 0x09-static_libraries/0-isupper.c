db555d0f7abb959ae827cac74a9ec1f06af63ef1
Abdoul azize <lepronet85@gmail.com>
2023-10-07 15:12:24 -0700

    Add create_static_lib.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,3 +1,17 @@
 #include "main.h"
 
-int _isupper(int c);
+/**
+ * _isupper - Function that check if upper
+ * @c: The letter to check
+ * Description: This function checks if letter passed is
+ * uppercase character
+ * Return: Reurn 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	return (0);
+}
