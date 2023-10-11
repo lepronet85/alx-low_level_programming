c4eb6e0b8b9f0ecd53e7974d1e8a26e11a006e2a
Abdoul azize <lepronet85@gmail.com>
2023-10-11 14:44:17 -0700

    Add 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
+
+/**
+ * _strlen - Function that return the length of sting
+ * @str: The string
+ * Return: String length
+ */
+
+int _strlen(const char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ * _strcopy - Copy the string
+ * @src: The source string
+ * Return: Copied string
+ */
+
+char *_strcopy(const char *src)
+{
+	int len = _strlen(src);
+	char *dest = (char *)malloc(len + 1);
+	int i = 0;
+
+	if (dest != NULL)
+	{
+		for (i = 0; i <= len; i++)
+			dest[i] = src[i];
+	}
+	return (dest);
+}
 
 /**
  * new_dog - Function that creates a new dog
@@ -11,23 +47,25 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
+	dog_t *doggo = (dog_t *)malloc(sizeof(dog_t));
 
-	if (new_dog == NULL)
+	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 
-	new_dog->name = strdup(name);
-	new_dog->owner = strdup(owner);
+	if (doggo == NULL)
+		return (NULL);
 
-	if (new_dog->name == NULL || new_dog->owner == NULL)
+	doggo->name = _strcopy(name);
+	doggo->age = age;
+	doggo->owner = _strcopy(owner);
+
+	if (doggo->name == NULL || doggo->owner == NULL)
 	{
-		free(new_dog->name);
-		free(new_dog->owner);
-		free(new_dog);
+		free(doggo->name);
+		free(doggo->owner);
+		free(doggo);
 		return (NULL);
 	}
 
-	new_dog->age = age;
-
-	return (new_dog);
+	return (doggo);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,12 +20,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
 
 #endif
