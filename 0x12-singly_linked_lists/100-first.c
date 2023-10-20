91168cd591e5b721c7f6c047842bf55e2e8b6f6a
Abdoul azize <lepronet85@gmail.com>
2023-10-19 17:46:44 -0700

    Add 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_message - Function that print message
+ * Return: Always nothing
+ */
+
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,4 +26,6 @@ list_t *add_node_end(list_t **head, const char *str);
 
 void free_list(list_t *head);
 
+void print_message(void) __attribute__((constructor));
+
 #endif
