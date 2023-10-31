fff69c0247031336cd9d11924fcb95bb25a76970
Abdoul azize <lepronet85@gmail.com>
2023-10-31 13:16:40 -0700

    Add 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	ssize_t text_length = 0;
-	ssize_t text_length = 0;
+	ssize_t bytes_written = write(fd, text_content, text_length);
 
 	if (filename == NULL)
 		return (-1);
