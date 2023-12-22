4286a0d83c3b5a0e7e6ac7ece137551dc3c28c39
Abdoul azize <lepronet85@gmail.com>
2023-12-22 14:33:52 -0800

    Add 4-hash_table_get.c

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -38,4 +38,6 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
+char *hash_table_get(const hash_table_t *ht, const char *key);
+
 #endif
