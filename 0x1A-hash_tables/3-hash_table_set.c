953b02b52b748a5b46882b4650d3e476717353f4
Abdoul azize <lepronet85@gmail.com>
2023-12-22 14:26:22 -0800

    Update 3-hash_table.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,57 +12,43 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node = NULL;
-	unsigned long int index;
-	char *new_value;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-	if (ht == NULL || key == NULL || strlen(key) == 0)
-		return 0;
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
-	if (ht->array[index] != NULL)
+	for (i = index; ht->array[i]; i++)
 	{
-		hash_node_t *temp = ht->array[index];
-		while (temp != NULL)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			if (strcmp(temp->key, key) == 0)
-			{
-				new_value = strdup(value);
-				if (new_value == NULL)
-					return (0);
-				free(temp->value);
-				temp->value = new_value;
-				return (1);
-			}
-			temp = temp->next;
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
 		}
 	}
 
-	new_node = malloc(sizeof(hash_node_t));
-
-	if (new_node == NULL)
-		return (0);
-
-	new_node->key = strdup(key);
-
-	if (new_node->key == NULL)
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
 	{
-		free(new_node);
+		free(value_copy);
 		return (0);
 	}
-
-	new_node->value = strdup(value);
-
-	if (new_node->value == NULL)
+	new->key = strdup(key);
+	if (new->key == NULL)
 	{
-		free(new_node->key);
-		free(new_node);
+		free(new);
 		return (0);
-    }
-
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
 	return (1);
 }
