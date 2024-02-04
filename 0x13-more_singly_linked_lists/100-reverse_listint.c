916f214db7df0ea076dedda9fef5663df5a4d4f2
lepronet85 <lepronet85@gmail.com>
2024-02-04 06:41:39 -0800

    Fix betty warning

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -3,7 +3,7 @@
 /**
  * reverse_listint - Reverses a listint_t linked list.
  * @head: Pointer to the head of the list
- * Returns: a pointer to the first node of the reversed list
+ * Return: a pointer to the first node of the reversed list
  */
 listint_t *reverse_listint(listint_t **head)
 {
