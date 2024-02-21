d0b18e8699151085840a2d05992e870fb0d362b9
lepronet85 <lepronet85@gmail.com>
2024-02-21 20:29:35 +0100

    Add 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Function to calculate the perimeter of the island described in a grid.
+"""
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island described in the grid.
+
+    Args:
+        grid (List[List[int]]): A list of lists of integers
+	representing the grid.
+
+    Returns:
+        int: The perimeter of the island.
+
+    Raises:
+        ValueError: If the grid is empty or invalid.
+    """
+    if not grid or not all(isinstance(row, list) for row in grid):
+        raise ValueError("Invalid grid")
+
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
