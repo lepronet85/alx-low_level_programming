c359daaa2563878903a23bee5da7db8b2693b38d
lepronet85 <lepronet85@gmail.com>
2024-04-11 21:04:32 +0100

    Update

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -39,7 +39,7 @@ int exponential_search(int *array, size_t size, int value)
 
 	printf("Value found between indexes [%lu] and [%lu]\n", low, high);
 
-	return binary_search(array, low, high, value);
+	return (binary_search(array, low, high, value));
 }
 
 /**
